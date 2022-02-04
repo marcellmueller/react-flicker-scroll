cc178e32de7c1acb260e7bfe589662e4f166f732
marcellmueller <mlmueller@protonmail.com>
2022-02-04 13:42:31 -0800

    Add license, add GitHub/NPM links to demo page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import styles from './App.module.css';
 import { Basic, FlickerImage } from './examples';
+import { GitHub, NPM } from './assets';
 
 function App() {
   return (
     <div className={styles.app}>
-      <Basic />
-      <FlickerImage />
+      <IconLink
+        alt="GitHub"
+        icon={GitHub}
+        text="View project on GitHub"
+        url="https://github.com/marcellmueller/react-flicker-scroll"
+      />
+      <IconLink
+        alt="NPM"
+        icon={NPM}
+        text="View project on NPM"
+        url="https://www.npmjs.com/package/react-flicker-scroll"
+      />
+      <div className={styles.container}>
+        <Basic />
+        <FlickerImage />
+      </div>
     </div>
   );
 }
 
+const IconLink = ({ alt, icon, text, url }) => {
+  return (
+    <a href={url}>
+      <div className={styles.link}>
+        <img src={icon} alt={alt} />
+        <span>{text}</span>
+      </div>
+    </a>
+  );
+};
 export default App;
